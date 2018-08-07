0b3f2f2cad984e7a19ad7f927264ba51e9d255f8
duncan <duncan@localhost.localdomain>
2018-08-07 16:30:42 +0300

    fix logging

---
+++ DSLWeb/src/main/java/com/healthit/Init.java
@@ -0,0 +1,48 @@
+package com.healthit;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ *
+ * @author duncan
+ */
+@WebServlet(name = "Init", urlPatterns = {"/Init"})
+public class Init extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Log4j Init Servlet is initializing");
+		String log4jLocation = config.getInitParameter("log4j-properties-location");
+
+		ServletContext sc = config.getServletContext();
+
+		if (log4jLocation == null) {
+			System.err.println("*** No log4j-location init param, so initializing log4j with BasicConfigurator");
+			BasicConfigurator.configure();
+		} else {
+			String webAppPath = sc.getRealPath("/");
+			String log4jProp = webAppPath + log4jLocation;
+			File loggingLocation = new File(log4jProp);
+			if (loggingLocation.exists()) {
+				System.out.println("Initializing log4j with: " + log4jProp);
+				PropertyConfigurator.configure(log4jProp);
+			} else {
+				System.err.println("*** " + log4jProp + " file not found, so initializing log4j with BasicConfigurator");
+				BasicConfigurator.configure();
+			}
+		}
+		super.init(config);
+	}
+
+}
