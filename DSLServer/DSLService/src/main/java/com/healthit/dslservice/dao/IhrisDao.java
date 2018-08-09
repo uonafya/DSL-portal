f0b644201efd7a381e0c0514becbb8ec2d8de0a2
duncan <duncan@localhost.localdomain>
2018-08-09 18:18:05 +0300

    filter ihris with missing perioid id

--- DSLServer/DSLService/src/main/java/com/healthit/dslservice/dao/IhrisDao.java
+++ DSLServer/DSLService/src/main/java/com/healthit/dslservice/dao/IhrisDao.java
@@ -27,7 +27,7 @@ public class IhrisDao {
     final static Logger log = Logger.getLogger(FacilityDao.class);
     private String getALlCadreGroup = "Select cadreid,cadrename from dim_ihris_cadre";
     private String getALlCadre = "Select dataelementid as id,dataelementname as cadrename, cadreid as cadre_group_id from dim_ihris_dataelement";
-    private String getCadreAllocation = "Select dataelementid as cadreid,periodid,mflcode,value from fact_ihris_datavalue";
+    private String getCadreAllocation = "Select dataelementid as cadreid,periodid,mflcode,value from fact_ihris_datavalue where periodid is not null";
    
     
     public List<CadreAllocation> getCadreAllocation() {
@@ -40,6 +40,8 @@ public class IhrisDao {
                 cadreAllocationList.setCadreid(rs.getString("cadreid"));
                 cadreAllocationList.setCadreNumber(rs.getString("value"));
                 cadreAllocationList.setMflcode(rs.getString("mflcode"));
+                cadreAllocationList.setPeriod(rs.getString("periodid"));
+                
                 cadreGroupList.add(cadreAllocationList);
             }
         } catch (SQLException ex) {
