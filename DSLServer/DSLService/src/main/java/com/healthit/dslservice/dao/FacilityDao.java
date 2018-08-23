b78a44749d85fa0d4f9986e3861f56ed4ba17ed2
duncan <duncan@localhost.localdomain>
2018-08-23 16:07:31 +0300

    added cacheing support

--- DSLServer/DSLService/src/main/java/com/healthit/dslservice/dao/FacilityDao.java
+++ DSLServer/DSLService/src/main/java/com/healthit/dslservice/dao/FacilityDao.java
@@ -8,11 +8,15 @@ package com.healthit.dslservice.dao;
 import com.healthit.dslservice.DslException;
 import com.healthit.dslservice.dto.KephLevel;
 import com.healthit.dslservice.dto.adminstrationlevel.Facility;
+import com.healthit.dslservice.util.CacheKeys;
 import com.healthit.dslservice.util.Database;
+import com.healthit.dslservice.util.DslCache;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
 import org.apache.log4j.Logger;
 
 /**
@@ -25,27 +29,45 @@ public class FacilityDao {
     private String getALlFacilties = "Select id,name,code as kmflcode,kephlevel_sk, owner_sk as owner_id, ward_id, sub_county_id "
             + "from facilities_facility";
 
+    Cache cache = DslCache.getCache();
+
     public List<Facility> getFacilities() throws DslException {
         List<Facility> facilityList = new ArrayList();
-        Database db =new Database();
-        ResultSet rs = db.executeQuery(getALlFacilties);
+        
         log.info("Fetching facilities");
-        try {
-            while (rs.next()) {
-                Facility facility = new Facility();
-                facility.setWardId(rs.getString("ward_id"));
-                facility.setFacilityOwner(rs.getString("owner_id"));
-                facility.setId(rs.getString("id"));
-                KephLevel l = KephLevel.getKephLevel(Integer.parseInt(rs.getString("kephlevel_sk")));
-                facility.setKephLevel(l);
-                facility.setName(rs.getString("name"));
-                facility.setSubCountyId(rs.getString("sub_county_id"));
-                facilityList.add(facility);
+        Element ele = cache.get(CacheKeys.facilityList);
+        String output = (ele == null ? null : ele.getObjectValue().toString());
+        //log.info("Element from cache " + output);
+        if (ele == null) {
+            long startTime = System.nanoTime();
+            Database db = new Database();
+            ResultSet rs = db.executeQuery(getALlFacilties);
+            try {
+                while (rs.next()) {
+                    Facility facility = new Facility();
+                    facility.setWardId(rs.getString("ward_id"));
+                    facility.setFacilityOwner(rs.getString("owner_id"));
+                    facility.setId(rs.getString("id"));
+                    KephLevel l = KephLevel.getKephLevel(Integer.parseInt(rs.getString("kephlevel_sk")));
+                    facility.setKephLevel(l);
+                    facility.setName(rs.getString("name"));
+                    facility.setSubCountyId(rs.getString("sub_county_id"));
+                    facilityList.add(facility);
+                }
+                cache.put(new Element(CacheKeys.facilityList, facilityList));
+
+            } catch (SQLException ex) {
+                log.error(ex);
+            } finally {
+                db.CloseConnection();
             }
-        } catch (SQLException ex) {
-            log.error(ex);
-        }finally{
-            db.CloseConnection();
+            long endTime = System.nanoTime();
+            log.info("Time taken to fetch data "+(endTime - startTime)/1000000);
+        } else {
+            long startTime = System.nanoTime();
+            facilityList=(List<Facility>) ele.getObjectValue();
+            long endTime = System.nanoTime();
+            log.info("Time taken to fetch data from cache "+(endTime - startTime)/1000000);
         }
         return facilityList;
     }
