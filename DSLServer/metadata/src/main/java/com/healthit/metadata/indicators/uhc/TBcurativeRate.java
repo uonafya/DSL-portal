a22cac139fef48bc38253487e4f0f0db9ef0b7f5
Duncan Ndiithi <duncanndiithi@yahoo.com>
2019-01-31 12:40:46 +0300

    implement yearly and monthly commodity metadeta fetchers

--- DSLServer/metadata/src/main/java/com/healthit/metadata/indicators/uhc/TBcurativeRate.java
+++ DSLServer/metadata/src/main/java/com/healthit/metadata/indicators/uhc/TBcurativeRate.java
@@ -33,6 +33,7 @@ public class TBcurativeRate implements Metadata {
         try {
             JSONObject orgUnitIdObj = (JSONObject) requestString.getOrgUnitID();
             orgUnitId = (String) orgUnitIdObj.getJSONArray(requestString.getOrgUnitType()).get(0);
+            log.debug("The or type is" + requestString.getOrgUnitType());
             if ("ward".equals(requestString.getOrgUnitType())) {
                 orgUnit = OrgUnitName.WARD;
             } else if ("subcounty".equals(requestString.getOrgUnitType())) {
@@ -57,7 +58,9 @@ public class TBcurativeRate implements Metadata {
         try {
             JSONObject period = (JSONObject) requestString.getPeriod();
             periodTypeRaw = requestString.getPeriodType();
-            if (periodType.equals("yearly")) {
+            log.debug("the period type is " + requestString.getPeriodType());
+
+            if (periodTypeRaw.equals("yearly")) {
                 periodType = PeriodType.YEARLY;
                 startYear = (String) period.getJSONArray("start_year").get(0);
                 endYear = (String) period.getJSONArray("end_year").get(0);
@@ -76,7 +79,7 @@ public class TBcurativeRate implements Metadata {
             log.error(ex);
         }
 
-        QueryParameters queryParams=new QueryParameters.QueryParametersBuilder()
+        QueryParameters queryParams = new QueryParameters.QueryParametersBuilder()
                 .setEndMonth(endMonth)
                 .setEndYear(endYear)
                 .setOrgId(orgUnitId)
@@ -85,19 +88,31 @@ public class TBcurativeRate implements Metadata {
                 .setOrgUnitName(orgUnit)
                 .setPeriodType(periodType)
                 .build();
-       
-        List<Object> components=responseManager(queryParams);
+
+        List<Object> components = responseManager(queryParams);
 
         return components;
     }
 
-    
-    private List<Object> responseManager(QueryParameters queryParams){
-        List<Object> components=null;
+    private List<Object> responseManager(QueryParameters queryParams) {
+        List<Object> components = null;
+        log.info("Running response manager");
+        log.info("period type " + queryParams.getPeriodType());
+        log.info("org unit type " + queryParams.getOrgUnitName());
+
+        if (queryParams.getPeriodType() == PeriodType.YEARLY && queryParams.getOrgUnitName() == OrgUnitName.NATIONAL) {
+            log.info("Yearly and National");
+            components = getNationalYearlyMetadata(queryParams.getStartYear(), queryParams.getEndYear(), queryParams.getStartMonth(), queryParams.getEndMonth());
+        }
+        if (queryParams.getPeriodType() == PeriodType.MONTHLY && queryParams.getOrgUnitName() == OrgUnitName.NATIONAL) {
+
+            log.info("Monthly and National");
+            components = getNationalMonthlyMetadata(queryParams.getStartYear(), queryParams.getEndYear(), queryParams.getStartMonth(), queryParams.getEndMonth());
+
+        }
         return components;
     }
-    
-    
+
     private String populateYearMonthParametersInQuery(String startYear, String endYear, String startMonth, String endMonth, String query) {
         query = query.replaceAll("@start_year@", startYear);
         query = query.replaceAll("@end_year@", endYear);
@@ -120,7 +135,7 @@ public class TBcurativeRate implements Metadata {
 
         QueryInterpreter queryIntpret = new QueryInterpreter();
         Map<String, List<Object>> data = queryIntpret.runSqlQuery(query);  // run query and bundle in data tables format
-      
+
         Map<String, Object> component = new HashMap();
         log.info("Final query " + query);
         component.put("display", "table");
@@ -172,13 +187,22 @@ public class TBcurativeRate implements Metadata {
         List<Object> components = new ArrayList();
         return components;
     }
-    
 
     private List<Object> getNationalMonthlyMetadata(String startYear, String endYear, String startMonth, String endMonth) {
+        String kemsaQuery = "commodity_sum_per_year_per_monthly";
+        return getCommodityData(startYear, endYear, startMonth, endMonth, kemsaQuery);
+    }
+
+    private List<Object> getNationalYearlyMetadata(String startYear, String endYear, String startMonth, String end_month) {
+        String kemsaQuery = "commodity_sum_year_range";
+        return getCommodityData(startYear, endYear, startMonth, end_month, kemsaQuery);
+    }
+
+    private List<Object> getCommodityData(String startYear, String endYear, String startMonth, String endMonth, String kemsaQuery) {
         Properties kemsaQueryFile = null;
         kemsaQueryFile = PropertiesLoader.getPropertiesFile(kemsaQueryFile, "kemsa.properties");
-        log.info("The query to run " + kemsaQueryFile.getProperty("commodity_sum_per_year_per_month"));
-        String query = kemsaQueryFile.getProperty("commodity_sum_per_year_per_month");
+        log.info("The query to run " + kemsaQueryFile.getProperty(kemsaQuery));
+        String query = kemsaQueryFile.getProperty(kemsaQuery);
         String commodityList = "['%ethambutol%','%isoniazid%', '%rifampicin%', '%pyrazinamide%']";
 
         query = query.replaceAll("@commodity_list@", commodityList);
@@ -187,10 +211,11 @@ public class TBcurativeRate implements Metadata {
         List<Object> components = new ArrayList();
 
         QueryInterpreter queryIntpret = new QueryInterpreter();
+        log.info("Final query tbcurative " + query);
         Map<String, List<Object>> data = queryIntpret.runSqlQuery(query);  // run query and bundle in data tables format
-      
+
         Map<String, Object> component = new HashMap();
-        log.info("Final query " + query);
+
         component.put("display", "table");
         component.put("data", data);
         log.info("All the keys kemsa " + PropertiesLoader.getAllKeys(kemsaQueryFile).toString());
@@ -206,36 +231,4 @@ public class TBcurativeRate implements Metadata {
         return components;
     }
 
-    private List<Object> getNationalYearlyMetadata(String startYear, String endYear, String startMonth, String endMonth) {
-        Properties kemsaQueryFile = null;
-        kemsaQueryFile = PropertiesLoader.getPropertiesFile(kemsaQueryFile, "kemsa.properties");
-        log.info("The query to run " + kemsaQueryFile.getProperty("commodity_sum_per_year_per_month"));
-        String query = kemsaQueryFile.getProperty("commodity_sum_per_year_per_month");
-        String commodityList = "['%ethambutol%','%isoniazid%', '%rifampicin%', '%pyrazinamide%']";
-
-        query = query.replaceAll("@commodity_list@", commodityList);
-        query = populateYearMonthParametersInQuery(startYear, endYear, startMonth, endMonth, query);
-
-        List<Object> components = new ArrayList();
-
-        QueryInterpreter queryIntpret = new QueryInterpreter();
-        Map<String, List<Object>> data = queryIntpret.runSqlQuery(query);  // run query and bundle in data tables format
-      
-        Map<String, Object> component = new HashMap();
-        log.info("Final query " + query);
-        component.put("display", "table");
-        component.put("data", data);
-        log.info("All the keys kemsa " + PropertiesLoader.getAllKeys(kemsaQueryFile).toString());
-
-        Map<String, String> dimension = new HashMap();
-        dimension.put("small", "6");
-        dimension.put("medium", "6");
-        dimension.put("large", "6");
-        component.put("dimension", dimension);
-
-        components.add(component);
-
-        return components;
-    }
-    
 }
